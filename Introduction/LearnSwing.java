624181970428017ce534e859fcb42be75119c42c
Ravi Prakash Chaurasiya <113404805+rp7007@users.noreply.github.com>
2023-03-20 22:45:02 +0530

    Add files via upload

---
+++ Introduction/LearnSwing.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class LearnSwing {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Introduction1");
+        frame.setLocation(200, 200);
+        frame.setSize(800, 800);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
